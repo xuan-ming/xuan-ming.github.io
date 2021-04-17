ac53249b42bb269a1b443d5ca23e3c1991bdf0a0
xuan-ming <1794005063@qq.com>
2021-04-18 04:21:29 +0800

    Site updated: 2021-04-18 04:21:28

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -9,7 +9,7 @@
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ噫又好啦~' + OriginTitle;
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
